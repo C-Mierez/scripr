62f5bee520d04808a8157d5543192a5aff1f1022
C-Mierez <carlos.mierez20@gmail.com>
2023-11-08 11:43:57 -0300

    🧑‍💻 New animation defaults

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -1,1 +1,6 @@
-export const transition = { duration: 0.8, ease: [0.76, 0, 0.24, 1] };
+export const bezierEase = [0.76, 0, 0.24, 1];
+export const shortDuration = 0.3;
+export const mediumDuration = 0.5;
+export const duration = 0.8;
+
+export const transition = { duration, bezierEase };
