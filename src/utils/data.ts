4f56f79554d23997eead805c8981e67404d24fb6
C-Mierez <carlos.mierez20@gmail.com>
2023-11-19 16:02:16 -0300

    ♻️ Extracted faq card data

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,3 +1,4 @@
+import { FAQCardData } from "~/app/_components/home/FAQCard";
 import { TableGroupData } from "~/app/_components/home/section/ComparisonSection";
 import { FeatureData } from "~/app/_components/home/section/FeaturesSection";
 import { PlanCardData } from "~/app/_components/home/section/PricingSection";
@@ -89,5 +90,38 @@ export const Content = {
                 ],
             },
         ] as TableGroupData[],
+    },
+    FAQ: {
+        FAQCardData: [
+            {
+                question: "What is a record?",
+                answer: "A record is a single entry in your accounting. It can be a transaction, an investment, a loan, a debt, an expense, etc.",
+            },
+            {
+                question: "What is a link?",
+                answer: "A link is a connection between two records. Each link has a type, which defines the nature of the connection.",
+            },
+            {
+                question: "How does our pricing work?",
+                answer: "Our transparent and self-serve model allows you to start small to keep your initial costs down and scale your analytics confidently as you grow. There are no overages if you exceed your plan, you simply pay for additional upgrades on the same cost per feature basis. You can go with one of our premade plans. Or if you need more help customizing an option that works for you, reach out to our sales team.",
+            },
+            {
+                question: "How can I estimate my amount of links?",
+                answer: "We recommend you to start with the Starter plan and upgrade to the Investor plan if you need more links. You can also build your own plan if you need more links but not the other features of the Investor plan.",
+            },
+            {
+                question: "Can I go over my plan limits?",
+                answer: "Yes, you can go over your plan limits. You will be charged for the extra links you use at the same cost per link as your plan. You can also upgrade to a higher plan if you need more links.",
+            },
+            {
+                question: "I am a student. Can I get a discount?",
+                answer: "Yes, we offer a free premium trial for your first year of use. Contact us to get your discount.",
+            },
+            { question: "Can I ask for my money back?", answer: "No." },
+            {
+                question: "Can I cancel my subscription?",
+                answer: "Yes, you can cancel your subscription at any time. You will still have access to your account until the end of your billing period.",
+            },
+        ] as FAQCardData[],
     },
 };
