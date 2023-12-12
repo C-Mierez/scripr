b0ab2af719e247ea2e0956cf78768db516a61785
C-Mierez <carlos.mierez20@gmail.com>
2023-12-12 12:04:46 -0300

    🐛 Fixed wrong animation curve field

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -1,8 +1,10 @@
+import { Transition } from "framer-motion";
+
 export const bezierEase = [0.76, 0, 0.24, 1];
 export const shortDuration = 0.3;
 export const mediumDuration = 0.5;
 export const duration = 0.8;
 
-export const transition = { duration, bezierEase };
+export const transition = { duration: duration, ease: bezierEase } as Transition;
 
 export const layoutTransition = { duration: shortDuration, bezierEase };
