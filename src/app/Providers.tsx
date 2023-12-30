fbe524688bb6c22da927ed806a8edc9f14621b8f
C-Mierez <carlos.mierez20@gmail.com>
2023-12-30 19:43:13 -0300

    🎨 Whitespace tabs

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -7,24 +7,26 @@ import { type ThemeProviderProps } from "next-themes/dist/types";
 // import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <ReactLenis
-      root={true}
-      options={{
-        lerp: 0.08,
-        touchMultiplier: 0.05,
-        smoothTouch: false,
-      }}
-    >
-      <ThemeProvider>{children}</ThemeProvider>
-    </ReactLenis>
-  );
+    return (
+        <ThemeProvider>
+            <ReactLenis
+                root={true}
+                options={{
+                    lerp: 0.08,
+                    touchMultiplier: 0.05,
+                    smoothTouch: false,
+                }}
+            >
+                {children}
+            </ReactLenis>
+        </ThemeProvider>
+    );
 }
 
 function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange {...props}>
-      {children}
-    </NextThemesProvider>
-  );
+    return (
+        <NextThemesProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange {...props}>
+            {children}
+        </NextThemesProvider>
+    );
 }
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,10 @@
 import DashboardHeader from "./_components/DashboardHeader";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <DashboardHeader />
-      {children}
-    </>
-  );
+    return (
+        <>
+            <DashboardHeader />
+            {children}
+        </>
+    );
 }
