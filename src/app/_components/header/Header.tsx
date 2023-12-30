16b603bf9748ba42bd2746e30e9a10e015307b39
C-Mierez <carlos.mierez20@gmail.com>
2023-12-30 15:39:04 -0300

    🚧 Utility hrefs for dashboard

--- src/app/_components/header/Header.tsx
+++ src/app/_components/header/Header.tsx
@@ -12,261 +12,261 @@ import css from "./Header.module.scss";
 import { headerBrandingVariants, headerVariants, menuHeightVariants, menuNavLinksVariants } from "./HeaderAnims";
 
 export default function Header() {
-    const navLinks = [
-        {
-            name: "Pricing",
-            href: `#${AnchorIDs.Pricing}`,
-        },
-        {
-            name: "Contact",
-            href: `#${AnchorIDs.Contact}`,
-        },
-        {
-            name: "Get Started",
-            href: "/",
-        },
-    ];
+  const navLinks = [
+    {
+      name: "Pricing",
+      href: `#${AnchorIDs.Pricing}`,
+    },
+    {
+      name: "Contact",
+      href: `#${AnchorIDs.Contact}`,
+    },
+    {
+      name: "Get Started",
+      href: "/dashboard",
+    },
+  ];
 
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    /* ---------------------------- Header Animations --------------------------- */
-    const [collapseBranding, setCollapseBranding] = useState(false);
-    const [collapseHeader, setCollapseHeader] = useState(true);
-    const collapsing = useRef(false);
-    const { scrollY } = useScroll();
-    const { height } = useDimensions();
-    const showBrandingThreshold = height * 0.2;
-    const showHeaderThreshold = height;
+  /* ---------------------------- Header Animations --------------------------- */
+  const [collapseBranding, setCollapseBranding] = useState(false);
+  const [collapseHeader, setCollapseHeader] = useState(true);
+  const collapsing = useRef(false);
+  const { scrollY } = useScroll();
+  const { height } = useDimensions();
+  const showBrandingThreshold = height * 0.2;
+  const showHeaderThreshold = height;
 
-    useMotionValueEvent(scrollY, "change", (latest) => {
-        // If Menu is open, ignore changes
-        if (isMenuOpen) return;
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    // If Menu is open, ignore changes
+    if (isMenuOpen) return;
 
-        const prev = scrollY.getPrevious();
+    const prev = scrollY.getPrevious();
 
-        /* --------------------------- Branding Thresholds -------------------------- */
-        if (collapseBranding && latest < showBrandingThreshold) {
-            setCollapseBranding(false);
-        } else if (!collapseBranding && latest > showBrandingThreshold) {
-            setCollapseBranding(true);
+    /* --------------------------- Branding Thresholds -------------------------- */
+    if (collapseBranding && latest < showBrandingThreshold) {
+      setCollapseBranding(false);
+    } else if (!collapseBranding && latest > showBrandingThreshold) {
+      setCollapseBranding(true);
+    }
+    /* ---------------------------- Header Thresholds --------------------------- */
+    if (latest > showHeaderThreshold) {
+      if (collapseHeader && prev > latest) {
+        setCollapseHeader(false);
+      } else if (!collapseHeader && !collapsing.current && prev < latest) {
+        collapsing.current = true;
+        setTimeout(() => {
+          setCollapseHeader(true);
+          collapsing.current = false;
+        }, 1000);
+      }
+    } else if (!collapseHeader && latest <= showHeaderThreshold) {
+      setCollapseHeader(true);
+    }
+  });
+  /* ---------------------------------- Lenis --------------------------------- */
+  const lenis = useLenis();
+
+  /* ------------------------------- Menu Toggle ------------------------------ */
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <motion.header
+      className={css.header}
+      onPointerLeave={() => {
+        if (scrollY.get() < showHeaderThreshold) {
+          setCollapseBranding(false);
+          setCollapseHeader(true);
         }
-        /* ---------------------------- Header Thresholds --------------------------- */
-        if (latest > showHeaderThreshold) {
-            if (collapseHeader && prev > latest) {
-                setCollapseHeader(false);
-            } else if (!collapseHeader && !collapsing.current && prev < latest) {
-                collapsing.current = true;
-                setTimeout(() => {
-                    setCollapseHeader(true);
-                    collapsing.current = false;
-                }, 1000);
+      }}
+      /* ------------------------------ FramerMotion ------------------------------ */
+      variants={headerVariants}
+      initial="initial"
+      animate={collapseHeader ? "collapse" : "expanded"}
+    >
+      <nav className={css.nav}>
+        <button onClick={toggleMenu}>{isMenuOpen ? "Close" : "Menu"}</button>
+
+        <motion.div
+          className={css.branding}
+          onClick={() => {
+            lenis.scrollTo(0);
+          }}
+          onPointerEnter={() => {
+            if (scrollY.get() < showHeaderThreshold) {
+              setCollapseBranding(true);
+              setCollapseHeader(false);
             }
-        } else if (!collapseHeader && latest <= showHeaderThreshold) {
-            setCollapseHeader(true);
-        }
-    });
-    /* ---------------------------------- Lenis --------------------------------- */
-    const lenis = useLenis();
-
-    /* ------------------------------- Menu Toggle ------------------------------ */
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
-
-    return (
-        <motion.header
-            className={css.header}
-            onPointerLeave={() => {
-                if (scrollY.get() < showHeaderThreshold) {
-                    setCollapseBranding(false);
-                    setCollapseHeader(true);
-                }
-            }}
-            /* ------------------------------ FramerMotion ------------------------------ */
-            variants={headerVariants}
-            initial="initial"
-            animate={collapseHeader ? "collapse" : "expanded"}
+          }}
+          /* ------------------------------ FramerMotion ------------------------------ */
+          variants={headerBrandingVariants}
+          initial="initial"
+          animate={collapseBranding ? "collapse" : "expanded"}
         >
-            <nav className={css.nav}>
-                <button onClick={toggleMenu}>{isMenuOpen ? "Close" : "Menu"}</button>
+          <div className={css.logo}>
+            <SVGComponent.ScriprLogo />
+          </div>
+          <p className={css.name}>SCRIPR</p>
+        </motion.div>
 
-                <motion.div
-                    className={css.branding}
-                    onClick={() => {
-                        lenis.scrollTo(0);
-                    }}
-                    onPointerEnter={() => {
-                        if (scrollY.get() < showHeaderThreshold) {
-                            setCollapseBranding(true);
-                            setCollapseHeader(false);
-                        }
-                    }}
-                    /* ------------------------------ FramerMotion ------------------------------ */
-                    variants={headerBrandingVariants}
+        <ul className={css.links}>
+          {navLinks.map((link, index) => (
+            <li key={`navItem${index}`}>
+              <AnimatePresence mode="wait">
+                {!isMenuOpen && (
+                  <motion.a
+                    key={`navLink${index}`}
+                    href={link.href}
+                    variants={menuNavLinksVariants}
                     initial="initial"
-                    animate={collapseBranding ? "collapse" : "expanded"}
-                >
-                    <div className={css.logo}>
-                        <SVGComponent.ScriprLogo />
-                    </div>
-                    <p className={css.name}>SCRIPR</p>
-                </motion.div>
-
-                <ul className={css.links}>
-                    {navLinks.map((link, index) => (
-                        <li key={`navItem${index}`}>
-                            <AnimatePresence mode="wait">
-                                {!isMenuOpen && (
-                                    <motion.a
-                                        key={`navLink${index}`}
-                                        href={link.href}
-                                        variants={menuNavLinksVariants}
-                                        initial="initial"
-                                        animate="enter"
-                                        exit="exit"
-                                        custom={{ index, length: navLinks.length, globalDelay: 0.5 }}
-                                        onClick={() => {
-                                            if (link.href.startsWith("#")) {
-                                                lenis.scrollTo(link.href);
-                                            }
-                                        }}
-                                    >
-                                        {link.name}
-                                    </motion.a>
-                                )}
-                            </AnimatePresence>
-                        </li>
-                    ))}
-                </ul>
-                <AnimatePresence mode="wait">
-                    {isMenuOpen && (
-                        <Menu
-                            isMenuOpen={isMenuOpen}
-                            callback={() => {
-                                setIsMenuOpen(false);
-                            }}
-                        />
-                    )}
-                </AnimatePresence>
-            </nav>
-        </motion.header>
-    );
+                    animate="enter"
+                    exit="exit"
+                    custom={{ index, length: navLinks.length, globalDelay: 0.5 }}
+                    onClick={() => {
+                      if (link.href.startsWith("#")) {
+                        lenis.scrollTo(link.href);
+                      }
+                    }}
+                  >
+                    {link.name}
+                  </motion.a>
+                )}
+              </AnimatePresence>
+            </li>
+          ))}
+        </ul>
+        <AnimatePresence mode="wait">
+          {isMenuOpen && (
+            <Menu
+              isMenuOpen={isMenuOpen}
+              callback={() => {
+                setIsMenuOpen(false);
+              }}
+            />
+          )}
+        </AnimatePresence>
+      </nav>
+    </motion.header>
+  );
 }
 
 function Menu({ isMenuOpen, callback }: { isMenuOpen?: boolean; callback: () => void }) {
-    const menuItems = [
-        {
-            name: "Home",
-            href: `#${AnchorIDs.Home}`,
-        },
-        {
-            name: "Features",
-            href: `#${AnchorIDs.Features}`,
-        },
-        {
-            name: "Pricing",
-            href: `#${AnchorIDs.Pricing}`,
-        },
-        {
-            name: "FAQ",
-            href: `#${AnchorIDs.FAQ}`,
-        },
-        {
-            name: "Contact",
-            href: `#${AnchorIDs.Contact}`,
-        },
-        {
-            name: "About",
-            href: `#${AnchorIDs.About}`,
-        },
-        {
-            name: "Get Started",
-            href: "/",
-        },
-    ];
+  const menuItems = [
+    {
+      name: "Home",
+      href: `#${AnchorIDs.Home}`,
+    },
+    {
+      name: "Features",
+      href: `#${AnchorIDs.Features}`,
+    },
+    {
+      name: "Pricing",
+      href: `#${AnchorIDs.Pricing}`,
+    },
+    {
+      name: "FAQ",
+      href: `#${AnchorIDs.FAQ}`,
+    },
+    {
+      name: "Contact",
+      href: `#${AnchorIDs.Contact}`,
+    },
+    {
+      name: "About",
+      href: `#${AnchorIDs.About}`,
+    },
+    {
+      name: "Get Started",
+      href: "/dashboard",
+    },
+  ];
 
-    const lenis = useLenis();
-    const isPresent = useIsPresent();
-    const [activeItem, setActiveItem] = useState(-1);
-    const defaultActive = 0;
+  const lenis = useLenis();
+  const isPresent = useIsPresent();
+  const [activeItem, setActiveItem] = useState(-1);
+  const defaultActive = 0;
 
-    useEffect(() => {
-        if (isPresent) {
-            setActiveItem(0);
-        } else {
-            setActiveItem(-1);
-        }
-    }, [isPresent]);
+  useEffect(() => {
+    if (isPresent) {
+      setActiveItem(0);
+    } else {
+      setActiveItem(-1);
+    }
+  }, [isPresent]);
 
-    return (
-        <motion.div
-            className={css.menu}
-            variants={menuHeightVariants}
-            initial="initial"
-            animate="enter"
-            exit="exit"
-            custom={{ length: menuItems.length }}
-        >
-            <ul className={css.menuList}>
-                {menuItems.map((item, index) => (
-                    <li
-                        key={`menuItem${index}`}
-                        className={activeItem === index ? css.active : ""}
-                        onPointerEnter={() => {
-                            if (isPresent) {
-                                setActiveItem(index);
-                            } else {
-                                setActiveItem(-1);
-                            }
-                        }}
-                        onClick={() => {
-                            setActiveItem(index);
-                            callback();
-                        }}
-                        onPointerLeave={() => {
-                            setActiveItem(defaultActive);
-                        }}
-                    >
-                        {isMenuOpen && (
-                            <span>
-                                <motion.a
-                                    key={`menuItemLink${index}`}
-                                    href={item.href}
-                                    onClick={() => {
-                                        if (item.href.startsWith("#")) {
-                                            lenis.scrollTo(item.href);
-                                        }
-                                    }}
-                                    /* ------------------------------ FramerMotion ------------------------------ */
-                                    variants={menuNavLinksVariants}
-                                    initial="initial"
-                                    animate="enter"
-                                    exit="exit"
-                                    custom={{ index, length: menuItems.length, globalDelay: 0 }}
-                                >
-                                    {item.name}
-                                </motion.a>
-                            </span>
-                        )}
-                    </li>
-                ))}
-            </ul>
-            <div className={css.foot}>
-                <ClickableClipboardSmallText />
-                <p>All your finances, in one single place.™</p>
-                <ul>
-                    <li>
-                        <a href="https://github.com/C-Mierez/scripr" target="_blank">
-                            <SVGComponent.GitHubLogo />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://twitter.com/CMierez_" target="_blank">
-                            <SVGComponent.XLogo />
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </motion.div>
-    );
+  return (
+    <motion.div
+      className={css.menu}
+      variants={menuHeightVariants}
+      initial="initial"
+      animate="enter"
+      exit="exit"
+      custom={{ length: menuItems.length }}
+    >
+      <ul className={css.menuList}>
+        {menuItems.map((item, index) => (
+          <li
+            key={`menuItem${index}`}
+            className={activeItem === index ? css.active : ""}
+            onPointerEnter={() => {
+              if (isPresent) {
+                setActiveItem(index);
+              } else {
+                setActiveItem(-1);
+              }
+            }}
+            onClick={() => {
+              setActiveItem(index);
+              callback();
+            }}
+            onPointerLeave={() => {
+              setActiveItem(defaultActive);
+            }}
+          >
+            {isMenuOpen && (
+              <span>
+                <motion.a
+                  key={`menuItemLink${index}`}
+                  href={item.href}
+                  onClick={() => {
+                    if (item.href.startsWith("#")) {
+                      lenis.scrollTo(item.href);
+                    }
+                  }}
+                  /* ------------------------------ FramerMotion ------------------------------ */
+                  variants={menuNavLinksVariants}
+                  initial="initial"
+                  animate="enter"
+                  exit="exit"
+                  custom={{ index, length: menuItems.length, globalDelay: 0 }}
+                >
+                  {item.name}
+                </motion.a>
+              </span>
+            )}
+          </li>
+        ))}
+      </ul>
+      <div className={css.foot}>
+        <ClickableClipboardSmallText />
+        <p>All your finances, in one single place.™</p>
+        <ul>
+          <li>
+            <a href="https://github.com/C-Mierez/scripr" target="_blank">
+              <SVGComponent.GitHubLogo />
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/CMierez_" target="_blank">
+              <SVGComponent.XLogo />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </motion.div>
+  );
 }
--- src/app/_components/home/section/HeroSection.tsx
+++ src/app/_components/home/section/HeroSection.tsx
@@ -11,60 +11,60 @@ import { HeroSectionVariants } from "./HeroSectionAnims";
 import { defaultAnim } from "~/utils/animations";
 
 export default function HeroSection() {
-    const heroSectionRef = useRef(null);
+  const heroSectionRef = useRef(null);
 
-    const { scrollYProgress } = useScroll({ target: heroSectionRef });
-    const { scrollY: viewportScrollY } = useScroll();
+  const { scrollYProgress } = useScroll({ target: heroSectionRef });
+  const { scrollY: viewportScrollY } = useScroll();
 
-    /* ------------------------------ Section Anim ------------------------------ */
-    const padding = useTransform(viewportScrollY, [0, 300], ["1.5lvh", "0lvh"]);
-    const borderRadius = useTransform(viewportScrollY, [0, 300], ["6lvh", "0lvh"]);
+  /* ------------------------------ Section Anim ------------------------------ */
+  const padding = useTransform(viewportScrollY, [0, 300], ["1.5lvh", "0lvh"]);
+  const borderRadius = useTransform(viewportScrollY, [0, 300], ["6lvh", "0lvh"]);
 
-    /* -------------------------------- Grid Anim ------------------------------- */
-    const backgroundSize = useTransform(scrollYProgress, [0, 1], ["100%", "120%"]);
+  /* -------------------------------- Grid Anim ------------------------------- */
+  const backgroundSize = useTransform(scrollYProgress, [0, 1], ["100%", "120%"]);
 
-    /* ------------------------------- Slogan Anim ------------------------------ */
-    const scale = useTransform(scrollYProgress, [0, 1], [1, 0.8]);
-    const opacity = useTransform(scrollYProgress, [0.2, 0.8], [1, 0]);
-    const translateY = useTransform(scrollYProgress, [0, 0.8], ["0%", "20%"]);
+  /* ------------------------------- Slogan Anim ------------------------------ */
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.8]);
+  const opacity = useTransform(scrollYProgress, [0.2, 0.8], [1, 0]);
+  const translateY = useTransform(scrollYProgress, [0, 0.8], ["0%", "20%"]);
 
-    return (
-        <>
-            <div className={sharedCss.anchor} id={AnchorIDs.Home} />
-            <div ref={heroSectionRef} className={css.heroWrapper}>
-                <motion.section className={css.hero} style={{ paddingInline: padding, paddingTop: padding }}>
-                    <div className={css.outerBackground} />
-                    <motion.div
-                        className={css.innerCard}
-                        style={{ borderTopLeftRadius: borderRadius, borderTopRightRadius: borderRadius }}
-                    >
-                        <div className={css.gridBackgroundWrapper}>
-                            <motion.div
-                                className={css.gridBackground}
-                                {...defaultAnim(HeroSectionVariants.GridBackground)}
-                                style={{ backgroundSize }}
-                            />
-                        </div>
-
-                        <motion.div className={css.slogan} style={{ translateY }}>
-                            <motion.h1 {...defaultAnim(HeroSectionVariants.SloganH1)} style={{ opacity, scale }}>
-                                All your Finances
-                            </motion.h1>
-                            <motion.h2 {...defaultAnim(HeroSectionVariants.SloganH2)} style={{ opacity, scale }}>
-                                in one single place
-                            </motion.h2>
-                        </motion.div>
-                        <div className={css.cta}>
-                            <SliderButton label="Get Started" href="#" />
-                            <motion.div className={css.description} {...defaultAnim(HeroSectionVariants.CTADesc)}>
-                                <p>Billed annually. Only 5$ a month.</p>
-                                <p>Join us now. Cancel any time.</p>
-                            </motion.div>
-                        </div>
-                    </motion.div>
-                </motion.section>
-                <div className={css.heroScrollSpacer} />
+  return (
+    <>
+      <div className={sharedCss.anchor} id={AnchorIDs.Home} />
+      <div ref={heroSectionRef} className={css.heroWrapper}>
+        <motion.section className={css.hero} style={{ paddingInline: padding, paddingTop: padding }}>
+          <div className={css.outerBackground} />
+          <motion.div
+            className={css.innerCard}
+            style={{ borderTopLeftRadius: borderRadius, borderTopRightRadius: borderRadius }}
+          >
+            <div className={css.gridBackgroundWrapper}>
+              <motion.div
+                className={css.gridBackground}
+                {...defaultAnim(HeroSectionVariants.GridBackground)}
+                style={{ backgroundSize }}
+              />
             </div>
-        </>
-    );
+
+            <motion.div className={css.slogan} style={{ translateY }}>
+              <motion.h1 {...defaultAnim(HeroSectionVariants.SloganH1)} style={{ opacity, scale }}>
+                All your Finances
+              </motion.h1>
+              <motion.h2 {...defaultAnim(HeroSectionVariants.SloganH2)} style={{ opacity, scale }}>
+                in one single place
+              </motion.h2>
+            </motion.div>
+            <div className={css.cta}>
+              <SliderButton label="Get Started" href="/dashboard" />
+              <motion.div className={css.description} {...defaultAnim(HeroSectionVariants.CTADesc)}>
+                <p>Billed annually. Only 5$ a month.</p>
+                <p>Join us now. Cancel any time.</p>
+              </motion.div>
+            </div>
+          </motion.div>
+        </motion.section>
+        <div className={css.heroScrollSpacer} />
+      </div>
+    </>
+  );
 }
