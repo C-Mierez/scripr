1a94ccac8ab3a5980d229737f48a625d447d86d3
C-Mierez <carlos.mierez20@gmail.com>
2024-01-03 02:03:05 -0300

    ♻️ Added more db queries and procedures

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,5 +1,15 @@
+import { eq } from "drizzle-orm";
 import { db as drizzleDB } from "./index";
+import { users } from "./schema";
+import { v4 as uuid } from "uuid";
 
+/* ---------------------------------- Utils --------------------------------- */
+export const isUserVerified = async (db: typeof drizzleDB, id: string) => {
+    const user = await getUserById(db, id);
+    return !!(user && user.emailVerified);
+};
+
+/* --------------------------------- Queries -------------------------------- */
 export const getUserByEmail = async (db: typeof drizzleDB, email: string) => {
     try {
         const user = await db.query.users.findFirst({
@@ -22,4 +32,32 @@ export const getUserById = async (db: typeof drizzleDB, id: string) => {
         console.error(error);
         return null;
     }
+};
+
+/* ------------------------------- Procedures ------------------------------- */
+export const createUser = async (
+    db: typeof drizzleDB,
+    {
+        email,
+        passwordHash,
+        passwordSalt,
+        name,
+    }: { email: string; passwordHash: string; passwordSalt: string; name: string }
+) => {
+    await db.insert(users).values({
+        id: uuid(),
+        email,
+        passwordHash,
+        passwordSalt,
+        name,
+    });
+};
+
+export const verifyUser = async (db: typeof drizzleDB, { id }: { id: string }) => {
+    await db
+        .update(users)
+        .set({
+            emailVerified: new Date(),
+        })
+        .where(eq(users.id, id));
 };
