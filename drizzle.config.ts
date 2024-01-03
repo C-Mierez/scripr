abdc9aac51d38f6951cd0348736742d34a4aa362
C-Mierez <carlos.mierez20@gmail.com>
2024-01-02 22:13:10 -0300

    🐛 Explicit migrations directory

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,11 @@ import { type Config } from "drizzle-kit";
 import { env } from "~/env.mjs";
 
 export default {
-  schema: "./src/server/db/schema.ts",
-  driver: "pg",
-  dbCredentials: {
-    connectionString: env.DATABASE_URL,
-  },
-  tablesFilter: ["test_*"],
+    schema: "./src/server/db/schema.ts",
+    out: "./drizzle",
+    driver: "pg",
+    dbCredentials: {
+        connectionString: env.DATABASE_URL,
+    },
+    tablesFilter: ["test_*"],
 } satisfies Config;
