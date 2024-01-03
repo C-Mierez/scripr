164b5eb198fcbe10312cd53f1aef9105a3c0b376
C-Mierez <carlos.mierez20@gmail.com>
2024-01-02 22:14:03 -0300

    🧑‍💻 Insert Roles to DB script

---
+++ setupDB.ts
@@ -0,0 +1,25 @@
+import { db } from "./src/server/db/index";
+import { roles } from "~/server/db/schema";
+import { UserRoleSchema } from "schemas";
+
+const main = async () => {
+    try {
+        console.log("Inserting roles on table roles...");
+
+        const roleValues = UserRoleSchema.Values;
+
+        await db.insert(roles).values([
+            {
+                name: roleValues.user,
+            },
+            {
+                name: roleValues.admin,
+            },
+        ]);
+        console.log("Inserted user and admin roles.");
+    } catch (error) {
+        console.log(error);
+    }
+    process.exit(0);
+};
+main();
