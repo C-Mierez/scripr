6483c6a86d892af5d1e5fbf26e7864f9f2607ca8
C-Mierez <carlos.mierez20@gmail.com>
2024-01-02 22:17:47 -0300

    ✨ SignUp procedure

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -1,33 +1,37 @@
-import { createTRPCRouter, publicProcedure } from "../trpc";
-import { LogInSchema, SignUpSchema } from "schemas";
 import { TRPCError } from "@trpc/server";
+import bcrypt from "bcryptjs";
+import { SignUpSchema } from "schemas";
+import { getUserByEmail } from "~/server/db/queries";
+import { users } from "~/server/db/schema";
+
+import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const authRouter = createTRPCRouter({
-    logIn: publicProcedure.input(LogInSchema).mutation(async ({ input }) => {
-        // Mock api random result
-        const randomResult = Math.random() > 0.5;
-        // if (!randomResult) {
-        //     throw new TRPCError({
-        //         code: "INTERNAL_SERVER_ERROR",
-        //         message: `Test error`,
-        //     });
-        // }
+    signUp: publicProcedure.input(SignUpSchema).mutation(async ({ ctx, input }) => {
+        // Confirm the email address is not already in use
+        const existingEmail = await getUserByEmail(ctx.db, input.email);
+        if (existingEmail) {
+            throw new TRPCError({
+                code: "BAD_REQUEST",
+                message: "Email address already in use.",
+            });
+        }
+        /* ------------------------------ Checks Passed ----------------------------- */
+        // Hash inputted password
+        const generatedSalt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(input.password, generatedSalt);
+        // Create the new user
+        await ctx.db.insert(users).values({
+            email: input.email,
+            passwordHash: hashedPassword,
+            passwordSalt: generatedSalt,
+            name: input.username,
+        });
+        // TODO: Send verification email
         return {
-            result: randomResult,
-        };
-    }),
-    signUp: publicProcedure.input(SignUpSchema).mutation(async ({ input }) => {
-        // Mock api random result
-        const randomResult = Math.random() > 0.5;
-        // if (!randomResult) {
-        //     throw new TRPCError({
-        //         code: "INTERNAL_SERVER_ERROR",
-        //         message: `Test error`,
-        //     });
-        // }
-        console.log(input);
-        return {
-            result: randomResult,
+            success: true,
         };
     }),
 });
+
+// https://trpc.io/docs/server/error-handling
