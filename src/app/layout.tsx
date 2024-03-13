307f727ab13c6cdf7c872b767969df5e9b564fe2
C-Mierez <carlos.mierez20@gmail.com>
2024-03-13 14:04:25 -0300

    ✨ Next loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { cookies } from "next/headers";
 import { TRPCReactProvider } from "~/trpc/react";
 import { Providers } from "./Providers";
 import { Toaster } from "@/components/ui/sonner";
+import NextTopLoader from "nextjs-toploader";
+import { duration } from "../utils/animations";
 
 const roboto = Roboto_Serif({
     subsets: ["latin"],
@@ -52,8 +54,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 className={`${playfair_Display.variable} ${playfair_Display_SC_400.variable} ${playfair_Display_SC_700.variable} ${cinzel.variable} ${roboto.variable} ${cinzelDecorative.variable} `}
             >
                 <Providers>
+                    <NextTopLoader
+                        zIndex={1000}
+                        speed={700}
+                        easing="cubic-bezier(0.76, 0, 0.24, 1)"
+                        color="var(--color-success)"
+                        showSpinner={false}
+                    />
                     <TRPCReactProvider cookies={cookies().toString()}>{children}</TRPCReactProvider>
-                    {/* <LenisScroller /> */}
                     <Toaster />
                 </Providers>
             </body>
