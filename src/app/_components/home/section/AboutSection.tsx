84cefbcd80f191d3ff902f17c402fadccbab7247
C-Mierez <carlos.mierez20@gmail.com>
2024-04-18 16:18:01 -0300

    💄 Simplified About section

--- src/app/_components/home/section/AboutSection.tsx
+++ src/app/_components/home/section/AboutSection.tsx
@@ -2,57 +2,18 @@
 
 import css from "./AboutSection.module.scss";
 import sharedCss from "../shared.module.scss";
-import { cv } from "~/lib/utils";
+import { cn } from "~/lib/utils";
 import { AnchorIDs } from "../../../../lib/data";
 import { useRef } from "react";
 import { motion, useMotionValueEvent, useScroll, useTransform } from "framer-motion";
 import { defaultAnim } from "~/lib/animations";
 
 export default function AboutSection() {
-    const firstSectionRef = useRef(null);
-    const secondSectionRef = useRef(null);
-
-    /* ------------------------------ First Section ----------------------------- */
-    const { scrollYProgress: firstScrollYProgress } = useScroll({
-        target: firstSectionRef,
-        offset: ["start center", "end center"],
-    });
-
-    const firstProgressBarHeight = useTransform(firstScrollYProgress, [0, 1], ["0%", "100%"]);
-
-    const firstTextsTranslateX = [
-        useTransform(firstScrollYProgress, [0, 0.15, 0.3], ["15%", "15%", "0%"]),
-        useTransform(firstScrollYProgress, [0, 0.45, 0.6], ["15%", "15%", "0%"]),
-    ];
-
-    const firstTextsOpacity = [
-        useTransform(firstScrollYProgress, [0, 0.15, 0.3], ["0", "0", "1"]),
-        useTransform(firstScrollYProgress, [0, 0.45, 0.6], ["0", "0", "1"]),
-    ];
-
-    /* ----------------------------- Second Section ----------------------------- */
-    const { scrollYProgress: secondScrollYProgress } = useScroll({
-        target: secondSectionRef,
-        offset: ["start center", "end center"],
-    });
-
-    const secondProgressBarHeight = useTransform(secondScrollYProgress, [0, 1], ["0%", "100%"]);
-
-    const secondTextsTranslateX = [
-        useTransform(secondScrollYProgress, [0, 0.15, 0.3], ["15%", "15%", "0%"]),
-        useTransform(secondScrollYProgress, [0, 0.45, 0.6], ["15%", "15%", "0%"]),
-    ];
-
-    const secondTextsOpacity = [
-        useTransform(secondScrollYProgress, [0, 0.15, 0.3], ["0", "0", "1"]),
-        useTransform(secondScrollYProgress, [0, 0.45, 0.6], ["0", "0", "1"]),
-    ];
-
     return (
         <>
             <div className={sharedCss.anchor} id={AnchorIDs.About} />
             <header className={css.about}>
-                <h1 className={cv(sharedCss.headerDecorated, sharedCss.alt)}>About Us</h1>
+                <h1 className={cn(sharedCss.headerDecorated, sharedCss.alt)}>About Us</h1>
                 <p>
                     We're a small but mighty team of people who are all about helping to make finance less overwhelming
                     and more accessible to people
@@ -60,65 +21,7 @@ export default function AboutSection() {
             </header>
 
             <section className={css.about}>
-                <div ref={firstSectionRef} className={css.timelineGrid}>
-                    <div />
-                    <motion.div
-                        className={css.timeline}
-                        style={{
-                            scaleY: firstProgressBarHeight,
-                        }}
-                    />
-                    <div className={css.timelineContent}>
-                        <motion.p
-                            style={{
-                                x: firstTextsTranslateX[0],
-                                opacity: firstTextsOpacity[0],
-                            }}
-                        >
-                            We know that keeping track of your money can be a hassle.
-                        </motion.p>
-                        <motion.p
-                            style={{
-                                x: firstTextsTranslateX[1],
-                                opacity: firstTextsOpacity[1],
-                            }}
-                        >
-                            Our goal is to help as many people as possible take control of their finances.
-                        </motion.p>
-                    </div>
-                </div>
-
                 <ScrollerSection />
-
-                <div ref={secondSectionRef} className={css.timelineGrid}>
-                    <div />
-                    <motion.div
-                        className={css.timeline}
-                        style={{
-                            scaleY: secondProgressBarHeight,
-                        }}
-                    ></motion.div>
-                    <div className={css.timelineContent}>
-                        <motion.p
-                            style={{
-                                x: secondTextsTranslateX[0],
-                                opacity: secondTextsOpacity[0],
-                            }}
-                        >
-                            We believe that everyone should have access to the tools and information they need to make
-                            smart financial moves.
-                        </motion.p>
-                        <motion.p
-                            style={{
-                                x: secondTextsTranslateX[1],
-                                opacity: secondTextsOpacity[1],
-                            }}
-                        >
-                            We strive to provide a clearer view of your financial picture, allowing you to take control
-                            of your finance.
-                        </motion.p>
-                    </div>
-                </div>
 
                 <motion.h1
                     {...defaultAnim({
@@ -131,7 +34,7 @@ export default function AboutSection() {
                             y: 0,
                         },
                     })}
-                    viewport={{ margin: "-20%", once: false }}
+                    viewport={{ margin: "0%", once: false }}
                 >
                     What are you waiting for?
                     <motion.div
@@ -187,8 +90,18 @@ function ScrollerSection() {
                     ></motion.div>
                 </motion.h2>
 
-                <div
-                    className={cv(css.textScroller, "bg-[var(--color-primary-600)] dark:bg-[var(--color-primary-800)]")}
+                <motion.div
+                    className={cn(css.textScroller, "bg-[var(--color-primary-600)] dark:bg-[var(--color-primary-800)]")}
+                    {...defaultAnim({
+                        initial: {
+                            y: "100%",
+                            opacity: 0,
+                        },
+                        inView: {
+                            y: "0%",
+                            opacity: 1,
+                        },
+                    })}
                 >
                     <motion.ul
                         className={css.scrollList}
@@ -201,7 +114,7 @@ function ScrollerSection() {
                         <li>INCOME</li>
                         <li>EARNINGS</li>
                     </motion.ul>
-                </div>
+                </motion.div>
             </div>
         </div>
     );
